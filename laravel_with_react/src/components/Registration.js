8a19c996074c86112b1fdfe71fc10d2ebb048b24
abdul <kaiumrk5896999@gmail.com>
2022-04-07 02:32:07 +0600

    this is third commit

--- laravel_with_react/src/components/Registration.js
+++ laravel_with_react/src/components/Registration.js
@@ -1,9 +1,58 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import axios from 'axios';
+//import React,{useState} from 'react';
  function Registration() {
+  const[iname,setIname]=useState("");
+  const[iemail,setIemail]=useState("");
+  const[ipassword,setIpassword]=useState("");
+   const Oname=(e)=>{
+      
+      setIname(e.target.value);
+      //console.log(setIname);
+  }
+  const Oemail=(e)=>{
+    setIemail(e.target.value);
+    //console.log(setIemail);
+}
+const Opassword=(e)=>{
+    setIpassword(e.target.value);
+    //console.log(setIpassword);
+}
+const onsubmit=(e)=>{
+    const obj={
+        name:iname,
+        email:iemail,
+        password:ipassword
+    }
+    console.log("Form is submitted");
+    console.log(obj);
+    e.preventDefault();
+    axios.post(`http://127.0.0.1:8000/api/register`,obj)
+    .then(response=>{
+        console.log(response.data)
+    })
+}
   return (
     <div>
         <h1>This is Registration Page</h1>
+        <form action='' onSubmit={onsubmit}>
+        <div>
+            <label>Enter Your Name</label>
+            <input type="text" name='name'  onChange={Oname} value={iname} ></input>
+        </div>
+        <div>
+            <label>Enter Your Email</label>
+            <input type="text" name='email'  onChange={Oemail} value={iemail}  ></input>
+        </div>
+        <div>
+            <label>Enter Your Password</label>
+            <input type="text" name='password'  onChange={Opassword} value={ipassword} ></input>
+        </div>
+        <div>
+            
+            <input type="submit" value={"Register"} ></input>
+        </div>
+        </form>
     </div>
   )
 }
--- laravel_with_react/src/components/ShowData.js
+++ laravel_with_react/src/components/ShowData.js
@@ -1,9 +1,63 @@
-import React from 'react'
-
+//import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
  function ShowData() {
+  const [userget,Setuser]=useState([]);
+  useEffect(()=>{
+    fetchUser();
+   },[])
+
+   function fetchUser(){
+    axios.get(`http://127.0.0.1:8000/api/datashow`)
+    .then(respone=>{
+         Setuser(respone.data);
+         //console.log(userget)
+     })
+     .catch(error=>{
+         console.log("Something is wrong");
+     })
+
+  }
+
+  function deleteUser(userid){
+    //alert(userid);
+
+    axios.delete(`http://127.0.0.1:8000/api/delete/${userid}`)
+    .then(respone=>{
+      console.log(respone.data);
+      console.log(userid);
+      fetchUser();
+    })
+    
+}
   return (
     <div>
         <h1>This is Show Data Page</h1>
+        <div>
+        <table className="table table-bordered">
+          <thead className="thead-dark">
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Password</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              userget.map((mydata)=>{
+               return <tr key={mydata.id}>
+                  <td>{mydata.id}</td>
+                    <td>{mydata.uname}</td>
+                    <td>{mydata.uemail}</td>
+                    <td>{mydata.upassword}</td>
+                    <td><button onClick={()=>deleteUser(mydata.id)}>Delete</button></td>
+                </tr>
+              })
+            }
+          </tbody>
+        </table>
+        </div>
     </div>
   )
 }
